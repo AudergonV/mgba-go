06a187f3d0c8d51c836eaa5146cdb644af57b534
Vincent Audergon <vincent.audergon@hes-so.ch>
2026-05-19 20:48:35 +0200

    feat: simple c function call

---
+++ pkg/mgba/mgba.h
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) Vincent Audergon
+ *
+ * This file is part of go-mgba, a Go binding for the mGBA emulator.
+ * It depends on libmgba (https://github.com/mgba-emu/mgba), which is
+ * also licensed under MPL-2.0.
+ */
+
+#ifndef MGBA_H
+#define MGBA_H
+
+void mgba_init();
+
+#endif // MGBA_H
